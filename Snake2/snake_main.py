d955069a8b1b530b1491826d183412e359c3b193
psandleben <119960345+psandleben@users.noreply.github.com>
2024-01-29 20:04:07 +0100

    Add files via upload

--- Snake2/snake_main.py
+++ Snake2/snake_main.py
@@ -2,27 +2,34 @@ import arcade
 
 SCREEN_WIDTH: int = 800
 SCREEN_HEIGHT: int = 600
-SCALE: float = 1.0
+SCALE: float = 0.5
 SCREEN_TITLE: str = "snake"
 MOVEMENT_SPEED: int = 16
 STARTING_DIRECTION: str = "south"
 
+SNAKEBODYEND_PATH = "Snake2/snake_body_end.png"
 SNAKEBODY_PATH = "Snake2/snake_body.png"
-SNAKEHEAD_PATH = "Snake2/snake_body2.png"
+SNAKEHEAD_PATH = "Snake2/snake_head.png"
 
 """----------------------------------------------------------------"""
 
 class SnakeBody(arcade.Sprite):
-    def __init__(self, filename: str, bodybefore, scale: float = 1.0):
+    def __init__(self, filename: str, bodybefore, scale: float = 1.0, center_x: int = 0, center_y: int = 0):
         super().__init__(filename, scale) 
         self.bodybefore = bodybefore
-        self.center_x_before = self.center_x
-        self.center_y_before = self.center_y
+        self.center_x = center_x
+        self.center_y = center_y
+    
     def move(self):
-        self.center_x_before = None
-        self.center_y_before = None
-        self.bodybefore.center_x_before = self.center_x
-        self.bodybefore.center_y_before = self.center_y
+        self.center_x = self.bodybefore.get_center_x()
+        self.center_y = self.bodybefore.get_center_y()
+
+    def get_center_x(self):
+        return self.center_x
+    def get_center_y(self):
+        return self.center_y
+    
+"""----------------------------------------------------------------"""
 
 class SnakeHead(arcade.Sprite):
     def __init__(self, filename: str, scale: float = 1.0, direction: str = "south"): 
@@ -31,13 +38,38 @@ class SnakeHead(arcade.Sprite):
 
     def move(self):
             if self.direction == "south":
-                self.center_y -= MOVEMENT_SPEED #runter bewegen
+                self.center_y += MOVEMENT_SPEED #runter bewegen
             elif self.direction == "north":
-                self.center_y += MOVEMENT_SPEED #hoch bewegen
+                self.center_y -= MOVEMENT_SPEED #hoch bewegen
             elif self.direction == "west":
                 self.center_x -= MOVEMENT_SPEED #links bewegen
             elif self.direction == "east":
                 self.center_x += MOVEMENT_SPEED #rechts bewegen
+    
+    def set_direction_left(self):
+        if self.direction == "south":
+            self.direction = "west"
+        elif self.direction == "west":
+            self.direction = "north"
+        elif self.direction == "north":
+            self.direction = "east"
+        elif self.direction == "east":
+            self.direction = "south"
+
+    def set_direction_right(self):
+        if self.direction == "south":
+            self.direction = "east"
+        elif self.direction == "east":
+            self.direction = "north"
+        elif self.direction == "north":
+            self.direction = "west"
+        elif self.direction == "west":
+            self.direction = "south"
+        
+    def get_center_x(self):
+        return self.center_x
+    def get_center_y(self):
+        return self.center_y
 
 """----------------------------------------------------------------"""
 
@@ -56,30 +88,33 @@ class MyGame(arcade.Window):
         self.time = None
         self.total_time = None
         self.esc_timer = None
+        self.snake_list_length = None
+        self.last_snake_list_body = None
 
     def setup(self):
         self.time = 0
         self.total_time = 0
         self.esc_timer = -1
+        self.snake_list_length = 3
 
         self.snake_list = arcade.SpriteList()
         self.snakehead = SnakeHead(SNAKEHEAD_PATH, SCALE, STARTING_DIRECTION)
         self.snakehead.center_x = SCREEN_WIDTH / 2
         self.snakehead.center_y = SCREEN_HEIGHT / 2
         self.snake_list.append(self.snakehead)
+        
+        snakebody_1 = SnakeBody(SNAKEBODY_PATH, self.snakehead, SCALE)
+        snakebody_1.center_x = SCREEN_WIDTH / 2 
+        snakebody_1.center_y = SCREEN_HEIGHT / 2 - 16*1
+        self.snake_list.append(snakebody_1)
 
-        self.snakebody_1 = SnakeBody(SNAKEBODY_PATH, self.snakehead, 1.0)
-        self.snakebody_1.center_x = SCREEN_WIDTH / 2 
-        self.snakebody_1.center_y = SCREEN_HEIGHT / 2 + 16*1
-        self.snake_list.append(self.snakebody_1)
-
-        self.snakebody_2 = SnakeBody(SNAKEBODY_PATH, self.snakebody_1, 1.0)
-        self.snakebody_2.center_x = SCREEN_WIDTH / 2
-        self.snakebody_2.center_y = SCREEN_HEIGHT / 2 + 16*2
-        self.snake_list.append(self.snakebody_2)
+        snakebody_2 = SnakeBody(SNAKEBODY_PATH, snakebody_1, SCALE)
+        snakebody_2.center_x = SCREEN_WIDTH / 2
+        snakebody_2.center_y = SCREEN_HEIGHT / 2 - 16*2
+        self.snake_list.append(snakebody_2)
+        self.last_snake_list_body = snakebody_2
         
         
-
     def on_draw(self):
         self.clear()
         self.snake_list.draw()
@@ -88,19 +123,32 @@ class MyGame(arcade.Window):
 
     def on_update(self, delta_time):
         self.snake_list.update()
-
         self.time += delta_time
         self.total_time += delta_time
-
-        if self.time >= 0.5:
-            self.snakehead.move()
-            self.snakebody_1.move()
-            self.snakebody_2.move()
+        
+        if self.time >= 0.5: 
+            i = self.snake_list_length -1
+            while i >= 0:
+                self.snake_list[i].move()
+                i -= 1
+            
             self.time = 0
 
     def on_key_press(self, key, key_modifiers):
         if key == arcade.key.ESCAPE:
             self.esc_timer = self.total_time
+        if key == arcade.key.LEFT:
+            self.snakehead.set_direction_left()
+        if key == arcade.key.RIGHT:
+            self.snakehead.set_direction_right()
+        if key == arcade.key.A:
+            snakebody = SnakeBody(SNAKEBODY_PATH, self.last_snake_list_body, SCALE)
+        snakebody.center_x = self.last_snake_list_body.get_center_x()
+        snakebody.center_y = self.last_snake_list_body.get_center_y()
+        self.snake_list.append(snakebody)
+        self.last_snake_list_body = snakebody
+        self.snake_list_length += 1
+            
 
     def on_key_release(self, key, key_modifiers):
         if key == arcade.key.ESCAPE:
